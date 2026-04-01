a28062e060818a182f1545f570209467015b8e24
Пользователь <vgutman95@mail.ru>
2026-04-01 11:34:58 +0500

    add site selector

--- src/js/main.js
+++ src/js/main.js
@@ -24,6 +24,8 @@ document.addEventListener('DOMContentLoaded', function() {
   initCounter();
   initMapDots();
 
+  initSiteSelector();
+
   initSliderConditions();
   ininInfoSlider();
   initBurger();
@@ -438,4 +440,66 @@ function initMapDots() {
       d.classList.remove('active');
     });
   });
+}
+
+
+/*Инициализация перелючания версии сайта */
+function initSiteSelector(){
+  const siteSelector = document.querySelector('.site__selector'),
+      items = siteSelector.querySelectorAll('li');
+
+  items.forEach(i=>{
+    i.classList.remove('active')
+    checkLang(i)
+    i.addEventListener('click', ()=>{
+      let langID = i.dataset.lang;
+      setCookie('lang', langID, {secure: true, 'max-age': 3600});
+      checkLang(i);
+      window.location.reload()
+    })
+  })
+
+
+  function checkLang(element){
+    if(getCookie('lang') != undefined) {
+      if(element.dataset.lang == getCookie('lang')){
+        element.classList.add('active')
+      }
+    }else{
+      setCookie('lang', 'ru', {secure: true, 'max-age': 3600});
+    }
+  }
+}
+
+
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function setCookie(name, value, options = {}) {
+
+  options = {
+    path: '/',
+    // при необходимости добавьте другие значения по умолчанию
+    ...options
+  };
+
+  if (options.expires instanceof Date) {
+    options.expires = options.expires.toUTCString();
+  }
+
+  let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+  for (let optionKey in options) {
+    updatedCookie += "; " + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += "=" + optionValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
 }
